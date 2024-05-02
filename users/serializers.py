bc48f20e9a406999400146f22fceb81eb4e67127
Umys <kipiani182@gmail.com>
2024-05-02 10:19:07 +0400

    init

---
+++ users/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from users.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Класс для сериализации пользователя"""
+
+    class Meta:
+        model = User
+        fields = ["id", "name", "email", "telegram_id"]
+
+
